ba6b4c5512a465063907025056ed35ac25bd38b2
ll9 <llwierer@web.de>
2019-02-15 22:23:29 +0100

    db context

---
+++ WindowsFormsApp4/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WindowsFormsApp4.Models;
+
+namespace WindowsFormsApp4.Data
+{
+    class ApplicationDbContext: DbContext
+    {
+        public DbSet<BaseEntity> BaseEntities { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=db.sqlite");
+        }
+    }
+}
