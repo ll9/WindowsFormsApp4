6c024142ce062c6b8c16faf88f187ebe1415c4b1
ll9 <llwierer@web.de>
2019-02-16 11:49:34 +0100

    basic gui

--- WindowsFormsApp4/Form1.Designer.cs
+++ WindowsFormsApp4/Form1.Designer.cs
@@ -28,13 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.AddTableButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Location = new System.Drawing.Point(13, 13);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(567, 321);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // AddTableButton
+            // 
+            this.AddTableButton.Location = new System.Drawing.Point(624, 13);
+            this.AddTableButton.Name = "AddTableButton";
+            this.AddTableButton.Size = new System.Drawing.Size(75, 23);
+            this.AddTableButton.TabIndex = 1;
+            this.AddTableButton.Text = "Add Table";
+            this.AddTableButton.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.AddTableButton);
+            this.Controls.Add(this.tabControl1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -42,6 +63,9 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.Button AddTableButton;
     }
 }
 
